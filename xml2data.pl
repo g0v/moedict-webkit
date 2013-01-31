2b3aed0e65e2860445227bacc0974f7833d50181
Audrey Tang <audreyt@audreyt.org>
2013-02-01 03:31:09 +0800

    * Move options.html to its own data file

--- xml2data.pl
+++ xml2data.pl
@@ -1,16 +1,18 @@
 #!/usr/bin/env perl
-# perl xml2data.pl < moedict-mac/moedict_templates/MoeDictionary.xml
 use utf8;
 use strict;
-my %idToTitle;
+die "Usage: perl xml2data.pl moedict-mac/moedict_templates/MoeDictionary.xml" unless @ARGV and -e $ARGV[0];
+
+my %titleToId;
 my $curId;
 my $out = '';
 my $count = 0;
-while (<>) {
+open my $XML, '<:utf8', $ARGV[0];
+while (<$XML>) {
     if (/<d:entry id="(\d+)" d:title="([^"]+)">/) {
         $count++;
         print "$count\n" unless $count % 100;
-        $idToTitle{$1} = $2;
+        $titleToId{$2} = $1;
         my $dir = $1 % 100; 
         # $dir = "0$dir" unless $dir > 9;
         mkdir "data/$dir" unless -d "data/$dir";
@@ -30,3 +32,11 @@ while (<>) {
         $out .= $_;
     }
 }
+close $XML;
+
+open my $OPTIONS, '>:utf8', "options.html";
+for my $title (sort keys %titleToId) {
+    my $id = $titleToId{$title};
+    print $OPTIONS qq[<option value="$title" id="$id" />\n];
+}
+close $OPTIONS;
