fec56ec24912bf35dcf5d66b284244f8bc96b2eb
Audrey Tang <audreyt@audreyt.org>
2013-07-22 22:42:44 +0800

    * Add config.rb to version control

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "."
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+#output_style = :compressed
+line_comments = false
+preferred_syntax = :scss
