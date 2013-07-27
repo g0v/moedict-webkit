be62530d117cf8ecbd53582c62616ad1f3bc9459
Audrey Tang <audreyt@audreyt.org>
2013-07-28 02:30:10 +0800

    * Use relative paths for fonts

--- config.rb
+++ config.rb
@@ -1,10 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "."
 css_dir = "."
 sass_dir = "sass"
 images_dir = "images"
+fonts_dir= "fonts"
 javascripts_dir = "javascripts"
 #output_style = :compressed
 line_comments = false
