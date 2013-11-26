daa6de4aeb07d0364bd0099fe119e9f17e946862
Audrey Tang <audreyt@audreyt.org>
2013-11-27 00:20:33 +0800

    * Various tweaks to the sass/jade settings

--- link2pack.pl
+++ link2pack.pl
@@ -3,7 +3,7 @@ use utf8;
 use 5.008;
 use Encode;
 my $lang = shift;
-unless ($lang ~~ [qw[ t a h c ]] and not -t STDIN) {
+unless ($lang =~ /^[tahc]/ and not -t STDIN) {
     die << '.';
 Please invoke this as one of:
     perl link2pack.pl a < a.txt
