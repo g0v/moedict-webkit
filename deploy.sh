0d2d351df6df2d6a328d1e6f9e70931ff840c010
Zhemin Lin <lin.zhemin@gmail.com>
2013-12-05 11:11:07 +0800

    Force to copy index.json, so auto-complete works.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,9 @@
 # Deploy moedict to remote
 DST=ckhis.ck.tp.edu.tw:public_html/moedict/
 
-rsync -avz --delete a about.html config.rb cordova.js fonts h images index.html js main.js sass styles.css t p n $DST
+rm -f p/index.json
+rm -f n/index.json
+cp nan/util/index.json n/
+cp amis-data/index.json p/
+
+rsync -avz --delete a about.html config.rb cordova.js css fonts h images index.html js main.js sass styles.css t p n $DST
