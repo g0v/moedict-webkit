f23c0357c768bb6d2ee162dd661ed6a8335b468b
Kang-min Liu <gugod@gugod.org>
2015-01-25 22:53:17 +0100

    make it work with hakka dict.

--- build-pinyin-lookup.pl
+++ build-pinyin-lookup.pl
@@ -7,6 +7,8 @@ use Unicode::Normalize;
 
 my $JSON = JSON->new->utf8->canonical;
 
+my $re_all_known_pinyin;
+
 sub insert_index {
     my ($ctx, $title, $terms) = @_;
     my $idx = $ctx->{pinyin_sans_tone};
@@ -73,6 +75,11 @@ mkdir "lookup";
 mkdir "lookup/pinyin";
 mkdir "lookup/pinyin/$lang";
 
+## ls -1 lookup/pinyin/a/*.json | cut -f 4 -d '/' | cut -f1 -d '.' | perl -MRegex::PreSuf=presuf -E 'my $re = presuf(<>); say $re'
+if ($lang eq "c") {
+    $re_all_known_pinyin = '(?:a(?:ir|n[gr]|[inor])|b(?:a(?:ir|n(?:gr|[gr])|or|[inor])|e(?:ir|n(?:gr|[gr])|[inr])|i(?:a(?:nr|or|[nor])|er|ng|[enr])|or|ur|[aiou])|c(?:a(?:ir|n[gr]|or|[inor])|e(?:ngr?|[nr])|h(?:a(?:ngr?|or|[inor])|e(?:n(?:gr|[gr])|[nr])|ir|o(?:ngr?|u)|u(?:a(?:ir|n(?:gr|[gr])|[inr])|or|[ainor])|[aeiu])|ir|o(?:ngr?|ur|u)|u(?:a(?:nr|[nr])|er|nr|or|[ino])|[aeiu])|d(?:a(?:ir|n(?:gr|[gr])|or|[inor])|e(?:ngr?|[inr])|i(?:a(?:nr|or|[nor])|er|ngr?|ur|[eru])|o(?:ngr?|ur|u)|u(?:a(?:nr|[nr])|er|ir|nr|or|[inor])|[aeiu])|e(?:ng|[nr])|f(?:a(?:n(?:gr|[gr])|[nr])|e(?:n(?:gr|[gr])|[inr])|ou|ur|[aou])|g(?:a(?:ir|n(?:gr|[gr])|or|[inor])|e(?:n(?:gr|[gr])|[inr])|o(?:ngr?|ur|u)|u(?:a(?:ir|n(?:gr|[gr])|[inr])|er|ir|nr|or|[ainor])|[aeu])|h(?:a(?:ir|n[gr]|or|[inor])|e(?:ir|ngr?|[inr])|o(?:ng|ur|u)|u(?:a(?:ir|n(?:gr|[gr])|[inr])|er|ir|nr|or|[ainor])|[aeu])|j(?:i(?:a(?:n[gr]|or|[nor])|er|n(?:gr|[gr])|ong|ur|[aenru])|u(?:a(?:nr|[nr])|er|[enr])|[iu])|k(?:a(?:ir|n[gr]|[inor])|e(?:ngr?|[nr])|o(?:ngr?|ur|u)|u(?:a(?:ir|n[gr]|[inr])|er|nr|[ainor])|[aeu])|l(?:a(?:n(?:gr|[gr])|or|[inor])|e(?:ngr?|[ir])|i(?:a(?:n(?:gr|[gr])|or|[nor])|er|ngr?|ur|[aenru])|o(?:ngr?|ur|u)|u(?:a(?:nr|[nr])|er|nr|or|[enor])|[aeiou])|m(?:a(?:n[gr]|or|[inor])|e(?:ir|n(?:gr|[gr])|[inr])|i(?:a(?:nr|or|[nor])|er|ngr?|[enru])|o[ru]|ur|[aeiou])|n(?:a(?:ngr?|or|[inor])|e(?:ng|[in])|i(?:a(?:n(?:gr|[gr])|or|[nor])|ng|ur|[enu])|o(?:ngr?|u)|u(?:a(?:nr|[nr])|er|[enor])|[aeiu])|ou|p(?:a(?:ir|n(?:gr|[gr])|or|[inor])|e(?:ir|n(?:gr|[gr])|[inr])|i(?:a(?:nr|or|[nor])|er|ng|[aenr])|o[ru]|ur|[aiou])|q(?:i(?:a(?:n(?:gr|[gr])|or|[nor])|er|n(?:gr|[gr])|ongr?|ur|[aenru])|u(?:a(?:nr|[nr])|er|[enr])|[iu])|r(?:a(?:ngr?|[no])|e(?:n[gr]|[nr])|ir|o(?:ng|ur|u)|u(?:a(?:nr|[nr])|[ino])|[eiu])|s(?:a(?:n(?:gr|[gr])|[inor])|e(?:ngr?|[inr])|h(?:a(?:ir|n(?:gr|[gr])|or|[inor])|e(?:n(?:gr|[gr])|[inr])|ir|our?|u(?:a(?:ngr?|[in])|er|ir|nr|or|[ainor])|[aeiu])|ir|o(?:ng|u)|u(?:an|er|ir|[ino])|[aeiu])|t(?:a(?:ir|n(?:gr|[gr])|or|[inor])|e(?:ngr?|r)|i(?:a(?:nr|or|[nor])|er|ngr?|[er])|o(?:ngr?|ur|u)|u(?:a(?:nr|[nr])|er|ir|[inor])|[aeiu])|w(?:a(?:n(?:gr|[gr])|[inr])|e(?:ir|n[gr]|[inr])|or|ur|[aou])|x(?:i(?:a(?:n(?:gr|[gr])|or|[nor])|er|n(?:gr|[gr])|ong|ur|[aenru])|u(?:a(?:nr|[nr])|er|nr|[enr])|[iu])|y(?:a(?:n(?:gr|[gr])|or|[inor])|er|i(?:n(?:gr|[gr])|[nr])|o(?:ng|ur|u)|u(?:a(?:nr|[nr])|er|[enr])|[aeiou])|z(?:a(?:ng|or|[inor])|e(?:ngr?|[inr])|h(?:a(?:n(?:gr|[gr])|or|[inor])|e(?:n(?:gr|[gr])|[inr])|ir|o(?:ngr?|ur|u)|u(?:a(?:n(?:gr|[gr])|[inr])|er|ir|nr|or|[ainor])|[aeiu])|ir|o(?:ngr?|u)|u(?:a(?:nr|[nr])|er|ir|or|[inor])|[aeiu])|[aeoq])';
+}
+
 my $dict = from_json(scalar read_file $dict_file, { binmode => ":utf8" });
 
 my %ctx = (
@@ -91,19 +98,15 @@ for (my $i = 0; $i < @$dict; $i++) {
     for my $heteronym (@{ $entry->{heteronyms} }) {
         next unless $heteronym->{pinyin};
 
-        my @pinyin_tokens = map {
-            split(/\s+/, $_)
-        } grep {
-            $_ ne '';
-        } map {
-            $_ = NFD($_);
-            s/ɑ/a/g;
-            s/([bcdfghjklmnpqrstwxyz])/ $1/ig;
-            s/^ +//;
-            $_;
-        } map {
-            split(/(?:\p{Punct}|<br>|陸⃝|陸|臺|-|[又語讀]音)/, $_)
-        } split /\s*\(變\)\s*/, $heteronym->{pinyin};
+        my @pinyin_tokens = grep { /\A[a-z]/ } split(/([a-z]+)/i, (NFD($heteronym->{pinyin}) =~ s/\p{Mark}//gr =~ s/ɑ/a/gr));
+
+        if (defined $re_all_known_pinyin) {
+            @pinyin_tokens = grep {
+                $_
+            } map {
+                split(/($re_all_known_pinyin)/)
+            } @pinyin_tokens;
+        }
 
         for my $p (@pinyin_tokens) {
             my $p0 = $p =~ s! $tone_re !!xgr =~ s/u\x{308}/v/gr;
@@ -112,7 +115,7 @@ for (my $i = 0; $i < @$dict; $i++) {
             $p1 =~ s{([1234])(\S+)}{$2$1}g;
 
             if ($p1 !~ /\A[ a-z1234]+\z/) {
-                say STDERR "This looks weird: $title $p";
+                say STDERR "This looks weird: $title: <$p>";
             } else {
                 insert_index( \%ctx, $title, [$p0]);
             }
