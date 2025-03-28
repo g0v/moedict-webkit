3eb3e4bd5206695238739accca76c1a02435a6f8
taniksin <jeremiah8822@gmail.com>
2025-03-28 22:36:52 +0800

    update language labels

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+// Header.jsx (範例內容)
+export const languageOptions = [
+  { code: 'zh', name: '華語辭典' },
+  { code: 'nan', name: '臺灣台語' },
+  { code: 'hak', name: '臺灣客語' }
+];
---
+++ src/i18n/messages.js
@@ -0,0 +1,6 @@
+// messages.js (範例內容)
+export default {
+  zh: "華語辭典",
+  nan: "臺灣台語",
+  hak: "臺灣客語"
+};
