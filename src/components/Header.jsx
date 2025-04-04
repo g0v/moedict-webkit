1ffce31812ecaa3512799f8f900de8115e00112f
taniksin <jeremiah8822@gmail.com>
2025-04-04 21:07:34 +0800

    Delete src directory
    刪除錯誤新增的檔案

--- src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-// Header.jsx (原始架構基礎上修改)
-export const languageOptions = [
-  { code: 'zh', name: '華語辭典' },
-  { code: 'nan', name: '臺灣台語' },
-  { code: 'hak', name: '臺灣客語' }
-];
--- src/i18n/messages.js
+++
@@ -1,6 +0,0 @@
-// messages.js (原始架構基礎上修改)
-export default {
-  zh: "華語辭典",
-  nan: "臺灣台語",
-  hak: "臺灣客語"
-};
